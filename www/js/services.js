e7f0fd0eb7af5d8b0583303d335451c0e6f91190
kevinfdas1213 <kevinstevenfloresgomez5@gmail.com>
2019-05-19 14:29:53 -0600

    kevin

--- www/js/services.js
+++ www/js/services.js
@@ -6,28 +6,28 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    name: 'Métodos hormonales',
+    lastText: 'Métodos combinados que contienen estrógeno y progestágeno y que se pueden administrar como píldoras, inyecciones, anillo vaginal o parche transdérmico. Métodos que contienen solo progestágeno y que se pueden administrar como píldoras, inyecciones, implantes, anillo vaginal o dispositivo intrauterino. Anticoncepción hormonal de emergencia que puede consistir en píldoras de levonorgestrel solo o en píldoras combinadas (Método de Yuzpe).',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
+    name: 'Métodos no hormonales reversibles',
+    lastText: 'Dispositivos intrauterinos. Anticonceptivos de barrera masculinos (condón) y femeninos (condón, diafragma; espermicidas).Método de la amenorrea de lactancia.Métodos de abstinencia periódica (moco cervical (Billings); calendario; temperatura basal; sintotérmico).',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    name: 'Métodos no hormonales permanentes:',
+    lastText: 'Anticoncepción quirúrgica voluntaria (esterilización) femenina.Anticoncepción quirúrgica voluntaria (esterilización) masculina.',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    name: '¿Cuáles son las características más importantes de los anticonceptivos?',
+    lastText: 'Los métodos anticonceptivos tienen diferentes características en las que hay que fijarse cuando se elija uno de ellos.Esto es necesario para que el método permita alcanzar las metas reproductivas en forma efectiva y segura para la salud y de acuerdo a la situación de vida.',
     face: 'img/perry.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
+    name: 'SEGURIDAD DE LOS MÉTODOS ANTICONCEPTIVOS',
+    lastText: 'La Organización Mundial de la Salud (OMS) con la colaboración de expertos(as) de otras organizaciones y agencias, revisa periódicamente la evidencia científica disponible sobre el efecto que los métodos anticonceptivos puedan tener sobre la salud de las personas que los usan. Basándose en esta evidencia se han formulado recomendaciones que se presentan en los Criterios Médicos de Elegibilidad para el Uso de Anticonceptivos y las Recomendaciones sobre Prácticas Seleccionadas para el Uso de Anticonceptivos.',
     face: 'img/mike.png'
   }];
 
